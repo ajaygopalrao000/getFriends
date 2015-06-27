50f027954823399ebdc1a6888d476b1eb0d51f0c
ajaygopalrao000 <Gurram@Gopals-MacBook-Pro.local>
2015-06-26 18:19:54 -0700

    Initial Commit

---
+++ getFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  getFriends
+//
+//  Created by Gopal Rao on 6/26/15.
+//  Copyright (c) 2015 Gopal Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
